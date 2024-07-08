5bb88f8ea788fd59875316ab00c5f8ce635ff5c4
BOND <mhashimkp@gmail.com>
2024-07-08 09:54:50 +0530

    Update 2665. Counter II.js

--- 2665. Counter II.js
+++ 2665. Counter II.js
@@ -3,6 +3,38 @@
  * @return { increment: Function, decrement: Function, reset: Function }
  */
 var createCounter = function (init) {
+    let val = init
+    const increment = () => {
+        return val += 1
+
+    }
+
+    const decrement = () => {
+        return val -= 1
+
+    }
+    const reset = () => {
+        return val = init
+
+    }
+    return { increment, reset, decrement }
+
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
+
+//----------------------------------------------------------------------------------------------------
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
     let count = init;
 
     return {
